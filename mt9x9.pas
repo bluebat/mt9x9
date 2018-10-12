8a719279adb91ccc826b6a5e84484ef0c5343e87
趙惟倫 <bluebat@member.fsf.org>
2018-10-12 13:33:40 +0800

    add mt9x9.pas,mt9x9.jl

---
+++ mt9x9.pas
@@ -0,0 +1,22 @@
+{ 9x9 multiplication table in Pascal }
+{ CC0, Wei-Lun Chao <bluebat@member.fsf.org>, 2018. }
+{ fpc mt9x9.pas ; ./mt9x9 }
+
+program mt9x9;
+uses SysUtils;
+var
+    i, j, k : Integer;
+begin
+    for i in [1, 4, 7] do
+    begin
+        for j := 1 to 9 do
+        begin
+            for k := i to i+2 do
+            begin
+                Write(Format('%dx%d=%2d'#9, [k, j, k*j]));
+            end;
+            Writeln;
+        end;
+        Writeln;
+    end;
+end.
