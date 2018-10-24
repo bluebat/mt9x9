fe1787c5d6547f3594bf63200c7a79c6afcf2184
趙惟倫 <bluebat@member.fsf.org>
2018-10-24 08:57:01 +0800

    better mt9x9.cob

--- mt9x9.cob
+++ mt9x9.cob
@@ -1,26 +1,26 @@
-      * 9x9 multiplication table in COBOL
-      * CC0, Wei-Lun Chao <bluebat@member.fsf.org>, 2018.
-      * cobc -x mt9x9.cob ; ./mt9x9
+*> 9x9 multiplication table in COBOL
+*> CC0, Wei-Lun Chao <bluebat@member.fsf.org>, 2018.
+*> cobc -Fxj mt9x9.cob || ( cobc -Fx mt9x9.cob ; ./mt9x9 )
 
-       identification division.
-       program-id. mt9x9.
-       data division.
-       working-storage section.
-       01 i pic 9.
-       01 j pic 9.
-       01 k pic 9.
-       01 l pic 9.
-       01 kj pic z9.
-       procedure division.
-       perform varying i from 0 by 3 until i greater than 8
-           perform with test after varying j from 1 by 1 until j equal 9
-               perform varying l from 1 by 1 until l greater than 3
-                   add i l giving k
-                   multiply k by j giving kj
-                   display k "x" j "=" kj x"09" with no advancing
-               end-perform
-               display x"00"
-           end-perform
-           display x"00"
-       end-perform
-       stop run.
+identification division.
+program-id. mt9x9.
+data division.
+working-storage section.
+01 i pic 9.
+01 j pic 9.
+01 k pic 9.
+01 l pic 9.
+01 kj pic z9.
+procedure division.
+perform varying i from 0 by 3 until i greater than 8
+    perform with test after varying j from 1 by 1 until j equal 9
+        perform varying l from 1 by 1 until l greater than 3
+            add i l giving k
+            multiply k by j giving kj
+            display k "x" j "=" kj x"09" with no advancing
+        end-perform
+        display x"00"
+    end-perform
+    display x"00"
+end-perform
+stop run.
