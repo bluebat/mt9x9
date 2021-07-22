2047b386388a03356b120452441247efe2bdb8c6
趙惟倫 <bluebat@member.fsf.org>
2021-07-22 09:33:18 +0800

    add mt9x9.pli,mt9x9.zig

---
+++ mt9x9.zig
@@ -0,0 +1,18 @@
+// 9x9 multiplication table in Zig
+// CC0, Wei-Lun Chao <bluebat@member.fsf.org>, 2021.
+// zig build-exe mt9x9.zig && ./mt9x9 || zig run mt9x9.zig
+
+const std = @import("std");
+pub fn main() !void {
+    var i: u8 = 1;
+    while (i < 10) : (i += 3) {
+        var j: u8 = 1;
+        while (j < 10) : (j += 1) {
+            for ([_]u8{i, i+1, i+2}) |k| {
+                std.debug.print("{d}x{d}={d: >2}\t", .{k, j, k*j});
+            }
+            std.debug.print("\n", .{});
+        }
+        std.debug.print("\n", .{});
+    }
+}
